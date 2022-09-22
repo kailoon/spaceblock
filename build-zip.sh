af04aa19bfbcb20823cb6585bfb6856433757c71
kailoon <chankailoon@gmail.com>
2022-09-22 16:09:34 +0800

    fixing header.html and footer.html

---
+++ build-zip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Enable nicer messaging for build status.
+BLUE_BOLD='\033[1;34m';
+GREEN_BOLD='\033[1;32m';
+RED_BOLD='\033[1;31m';
+YELLOW_BOLD='\033[1;33m';
+COLOR_RESET='\033[0m';
+error () {
+	echo -e "\n${RED_BOLD}$1${COLOR_RESET}\n"
+}
+status () {
+	echo -e "\n${BLUE_BOLD}$1${COLOR_RESET}\n"
+}
+success () {
+	echo -e "\n${GREEN_BOLD}$1${COLOR_RESET}\n"
+}
+warning () {
+	echo -e "\n${YELLOW_BOLD}$1${COLOR_RESET}\n"
+}
+
+# Exit if any command fails.
+set -e
+
+# Build the theme zip
+status "Creating theme zip... 🤐"
+
+zip -r spaceblook.zip assets/ parts/ patterns/ styles/ templates/ *.php readme.txt screenshot.png theme.json style.css
+
+status "You've built the theme ✅"

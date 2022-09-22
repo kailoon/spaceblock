7aadfa8ec66a2aa0d11aa9f4798e1931b325a8db
kailoon <chankailoon@gmail.com>
2022-09-22 14:15:19 +0800

    adding author card and newsletter join card

---
+++ patterns/author-card.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Title: Author Card
+ * Slug: spaceblock/author-card
+ * Categories: spaceblock-section
+ */
+?>
+<!-- wp:group {"tagName":"section","align":"wide","layout":{"inherit":false,"contentSize":"","wideSize":""}} -->
+<section class="wp-block-group alignwide">
+  <!-- wp:group {"layout":{"contentSize":"1024px"}} -->
+  <div class="wp-block-group">
+    <!-- wp:columns {"style":{"spacing":{"blockGap":"40px","padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}},"border":{"width":"2px"}}} -->
+    <div class="wp-block-columns"
+      style="border-width:2px;padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px">
+      <!-- wp:column {"width":"20%"} -->
+      <div class="wp-block-column" style="flex-basis:20%">
+        <!-- wp:image {"id":1908,"width":171,"height":256,"sizeSlug":"large","linkDestination":"none"} -->
+        <figure class="wp-block-image size-large is-resized"><img
+            src="http://block-dev.test/wp-content/uploads/2022/09/m86psdx7olo-684x1024.jpg"
+            alt="woman in black scoop neck shirt" class="wp-image-1908" width="171" height="256" /></figure>
+        <!-- /wp:image -->
+      </div>
+      <!-- /wp:column -->
+
+      <!-- wp:column {"width":"80%"} -->
+      <div class="wp-block-column" style="flex-basis:80%">
+        <!-- wp:heading {"level":3,"style":{"color":{"text":"#979797"}},"fontSize":"base"} -->
+        <h3 class="has-text-color has-base-font-size" style="color:#979797">Darrell Steward</h3>
+        <!-- /wp:heading -->
+
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"textColor":"quaternary","fontSize":"md"} -->
+        <p class="has-quaternary-color has-text-color has-md-font-size" style="font-style:normal;font-weight:700">
+          Twitter specialist. Zombie enthusiast. Tv fanatic. Lifelong gamer. Food geek. Wannabe travel nerd. Pop culture
+          evangelist.</p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:column -->
+
+      <!-- wp:column {"width":"20%"} -->
+      <div class="wp-block-column" style="flex-basis:20%">
+        <!-- wp:buttons -->
+        <div class="wp-block-buttons">
+          <!-- wp:button -->
+          <div class="wp-block-button"><a class="wp-block-button__link" href="#">View Author</a></div>
+          <!-- /wp:button -->
+        </div>
+        <!-- /wp:buttons -->
+      </div>
+      <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+  </div>
+  <!-- /wp:group -->
+</section>
+<!-- /wp:group -->
---
+++ patterns/join-card.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Title: Join
+ * Slug: spaceblock/join-card
+ * Categories: spaceblock-section
+ */
+?>
+<!-- wp:group {"tagName":"section","align":"wide","layout":{"inherit":false,"contentSize":"","wideSize":""}} -->
+<section class="wp-block-group alignwide">
+  <!-- wp:group {"layout":{"contentSize":"1024px"}} -->
+  <div class="wp-block-group">
+    <!-- wp:group {"style":{"spacing":{"padding":{"top":"60px","right":"60px","bottom":"60px","left":"60px"}}},"backgroundColor":"black","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+    <div class="wp-block-group has-black-background-color has-background"
+      style="padding-top:60px;padding-right:60px;padding-bottom:60px;padding-left:60px">
+      <!-- wp:group {"style":{"spacing":{"blockGap":"0px"}}} -->
+      <div class="wp-block-group">
+        <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"700","letterSpacing":"-1px"}},"textColor":"white","fontSize":"lg"} -->
+        <p class="has-white-color has-text-color has-lg-font-size"
+          style="font-style:normal;font-weight:700;letter-spacing:-1px">Get into the space and start flowing</p>
+        <!-- /wp:paragraph -->
+
+        <!-- wp:paragraph {"textColor":"senary","fontSize":"sm"} -->
+        <p class="has-senary-color has-text-color has-sm-font-size">Join the community to explore the space together.
+        </p>
+        <!-- /wp:paragraph -->
+      </div>
+      <!-- /wp:group -->
+
+      <!-- wp:buttons -->
+      <div class="wp-block-buttons">
+        <!-- wp:button {"backgroundColor":"white","textColor":"black","className":"is-style-fill"} -->
+        <div class="wp-block-button is-style-fill"><a
+            class="wp-block-button__link has-black-color has-white-background-color has-text-color has-background">Join
+            -></a></div>
+        <!-- /wp:button -->
+      </div>
+      <!-- /wp:buttons -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
+</section>
+<!-- /wp:group -->
